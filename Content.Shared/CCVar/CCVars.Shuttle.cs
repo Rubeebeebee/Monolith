d2abee6b977f53014c1157c95cceb9754644c17e
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-07-18 13:00:17 +0000

    chore: automatically update REUSE headers

--- Content.Shared/CCVar/CCVars.Shuttle.cs
+++ Content.Shared/CCVar/CCVars.Shuttle.cs
@@ -1,4 +1,13 @@
-﻿using Content.Shared.Administration;
+// SPDX-FileCopyrightText: 2024 metalgearsloth
+// SPDX-FileCopyrightText: 2025 Ark
+// SPDX-FileCopyrightText: 2025 GreaseMonk
+// SPDX-FileCopyrightText: 2025 Ilya246
+// SPDX-FileCopyrightText: 2025 Redrover1760
+// SPDX-FileCopyrightText: 2025 Simon
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.Administration;
 using Content.Shared.CCVar.CVarAccess;
 using Robust.Shared.Configuration;
 
